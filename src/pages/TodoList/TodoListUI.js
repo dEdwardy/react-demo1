749db49b353361b0881f4890d4d6bd8e2043ecef
Edw4rd6731 <957196958@qq.com>
2020-05-06 21:17:05 +0800

    无状态组件优化

--- src/pages/TodoList/TodoListUI.js
+++ src/pages/TodoList/TodoListUI.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Button } from 'antd'
 
-class TodoListUI extends Component {
-  render() {
-    return (
-      <div className="todo-list-wrapper">
-        <Input placeholder="Please Input" onChange={this.props.handleInputChange} value={this.props.inputValue} style={{ margin: '10px', width: '400px' }} />
-        <Button onClick={this.props.handleClickAdd} type="primary">add</Button>
-        <div className="list" style={{ position: 'relative' }}>
-          <ol>
-            {
-              this.props.todoList.map((item, index) => {
-                return (
-                  <li style={{ margin: '12px 0' }} key={index}><span>{item.content} </span>
-                    <span style={{ position: 'absolute', left: '400px' }}>
-                      <Button size="small" style={{ display: item.complete ? 'none' : '' }} onClick={() => this.props.handleToggle(index)} >完成</Button>
-                      <Button size="small" style={{ display: item.complete ? '' : 'none' }} onClick={() => this.props.handleToggle(index)} >未完成</Button>
-                    </span>
-                  </li>
-                )
-              })
-            }
-          </ol>
-        </div>
-      </div >
-    )
-  }
+const TodoListUI = (props) => {
+  return (
+    <div className="todo-list-wrapper">
+      <Input placeholder="Please Input" onChange={props.handleInputChange} value={props.inputValue} style={{ margin: '10px', width: '400px' }} />
+      <Button onClick={props.handleClickAdd} type="primary">add</Button>
+      <div className="list" style={{ position: 'relative' }}>
+        <ol>
+          {
+            props.todoList.map((item, index) => {
+              return (
+                <li style={{ margin: '12px 0' }} key={index}><span>{item.content} </span>
+                  <span style={{ position: 'absolute', left: '400px' }}>
+                    <Button size="small" style={{ display: item.complete ? 'none' : '' }} onClick={() => props.handleToggle(index)} >完成</Button>
+                    <Button size="small" style={{ display: item.complete ? '' : 'none' }} onClick={() => props.handleToggle(index)} >未完成</Button>
+                  </span>
+                </li>
+              )
+            })
+          }
+        </ol>
+      </div>
+    </div >
+  )
 }
 
 export default TodoListUI;
